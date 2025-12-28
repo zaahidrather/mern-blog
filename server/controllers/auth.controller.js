16e21b6a668e31cde6eb4125167ad007ad550299
zahid <zaahidrather@gmail.com>
2025-12-29 01:08:31 +0530

    Create SignIn api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { createError } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -36,4 +37,37 @@ const signUp = async (req, res, next) => {
   }
 };
 
-export { signUp };
+const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    return next(createError(400, "All fields are required"));
+  }
+
+  const validUser = await User.findOne({ email });
+
+  if (!validUser) {
+    return next(createError(400, "Invalid credentials"));
+  }
+
+  const { password: hashedPassword, ...userDetails } = validUser._doc;
+
+  const validPassword = await bcryptjs.compare(password, hashedPassword);
+
+  if (!validPassword) {
+    return next(createError(400, "Invalid credentials"));
+  }
+
+  const payload = {
+    id: validUser._id,
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "5s" });
+
+  res
+    .status(200)
+    .cookie("token", token, { httpOnly: true, maxAge: 5 * 1000 })
+    .json({ userDetails });
+};
+
+export { signUp, signIn };
