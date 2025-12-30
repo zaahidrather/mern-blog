a80ff3d8c83c77980dc8a6fb0fd9e4716342a0d7
zahid <zaahidrather@gmail.com>
2025-12-30 16:21:39 +0530

    Add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./user/userSlice.js";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+// 1. NESTED CONFIG: Put this here to control the "user" slice specifically
+const userPersistConfig = {
+  key: "user",
+  storage,
+  blacklist: ["error", "loading"],
+};
+
+// 2. WRAP THE REDUCER: Use persistReducer on the userReducer specifically
+const rootReducer = combineReducers({
+  user: persistReducer(userPersistConfig, userReducer),
+});
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export const persistor = persistStore(store);
