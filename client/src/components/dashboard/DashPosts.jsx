1bb94ed8f362903d6ae6b4e22ef1a5bc975aac5e
zahid <zaahidrather@gmail.com>
2026-04-08 10:51:59 +0530

    Add show more functionality to the post results of the dashboard

--- client/src/components/dashboard/DashPosts.jsx
+++ client/src/components/dashboard/DashPosts.jsx
@@ -4,7 +4,6 @@ import { useSelector } from 'react-redux';
 import {
 	Table,
 	TableBody,
-	TableCaption,
 	TableCell,
 	TableHead,
 	TableHeader,
@@ -15,9 +14,11 @@ import { Link } from 'react-router-dom';
 export default function DashPosts() {
 	const { currentUser } = useSelector((state) => state.user);
 	const [posts, setPosts] = useState([]);
+	const [showMore, setShowMore] = useState(true);
 
 	useEffect(() => {
 		async function fetchPosts() {
+			console.log('fetch posts');
 			try {
 				const res = await axios.get(`/api/post/getposts?id=${currentUser._id}`);
 				setPosts(res.data.posts);
@@ -30,68 +31,92 @@ export default function DashPosts() {
 		}
 	}, [currentUser._id]);
 
+	async function handleShowMore() {
+		const startIndex = posts.length;
+		console.log('posts.length', posts.length);
+		const res = await axios.get(
+			`/api/post/getposts?id=${currentUser._id}&startIndex=${startIndex}`,
+		);
+		setPosts((prev) => [...prev, ...res.data.posts]);
+		if (res.data.posts.length < 9) {
+			console.log('less than');
+			setShowMore(false);
+		}
+	}
+
 	return (
 		<div className="w-full overflow-x-auto p-3">
 			{currentUser.isAdmin && posts.length > 0 ? (
-				<Table className="border">
-					{/* <TableCaption>Caption here.</TableCaption> */}
-					<TableHeader>
-						<TableRow>
-							<TableHead>Date updated</TableHead>
-							<TableHead>Post image</TableHead>
-							<TableHead>Post title</TableHead>
-							<TableHead>Category</TableHead>
-							<TableHead>Delete</TableHead>
-							<TableHead className="text-right">
-								<span className="sr-only">Edit</span>{' '}
-								{/* sr-only keeps it accessible but invisible if you prefer the clean look */}
-								Edit
-							</TableHead>
-						</TableRow>
-					</TableHeader>
-					<TableBody>
-						{posts.map((post) => (
-							<TableRow key={post._id}>
-								<TableCell className="font-medium">
-									{new Date(post.updatedAt).toLocaleDateString()}
-								</TableCell>
-
-								<TableCell>
-									<Link to={`/post/${post.slug}`}>
-										<img
-											src={post.image}
-											alt={post.title}
-											className="h-10 w-20 rounded bg-gray-500 object-cover"
-										/>
-									</Link>
-								</TableCell>
-
-								<TableCell>
-									<Link
-										className="font-medium text-gray-900 hover:underline dark:text-white"
-										to={`/post/${post.slug}`}
-									>
-										{post.title}
-									</Link>
-								</TableCell>
-
-								<TableCell className="capitalize">{post.category}</TableCell>
-
-								<TableCell>
-									<span className="cursor-pointer font-medium text-red-500 hover:underline">
-										Delete
-									</span>
-								</TableCell>
-
-								<TableCell className="text-right">
-									<Link className="text-teal-500 hover:underline" to={`/update-post/${post._id}`}>
-										Edit
-									</Link>
-								</TableCell>
+				<>
+					<Table className="border">
+						{/* <TableCaption>Caption here.</TableCaption> */}
+						<TableHeader>
+							<TableRow>
+								<TableHead>Date updated</TableHead>
+								<TableHead>Post image</TableHead>
+								<TableHead>Post title</TableHead>
+								<TableHead>Category</TableHead>
+								<TableHead>Delete</TableHead>
+								<TableHead className="text-right">
+									<span className="sr-only">Edit</span>{' '}
+									{/* sr-only keeps it accessible but invisible if you prefer the clean look */}
+									Edit
+								</TableHead>
 							</TableRow>
-						))}
-					</TableBody>
-				</Table>
+						</TableHeader>
+						<TableBody>
+							{posts.map((post) => (
+								<TableRow key={post._id}>
+									<TableCell className="font-medium">
+										{new Date(post.updatedAt).toLocaleDateString()}
+									</TableCell>
+
+									<TableCell>
+										<Link to={`/post/${post.slug}`}>
+											<img
+												src={post.image}
+												alt={post.title}
+												className="h-10 w-20 rounded bg-gray-500 object-cover"
+											/>
+										</Link>
+									</TableCell>
+
+									<TableCell>
+										<Link
+											className="font-medium text-gray-900 hover:underline dark:text-white"
+											to={`/post/${post.slug}`}
+										>
+											{post.title}
+										</Link>
+									</TableCell>
+
+									<TableCell className="capitalize">{post.category}</TableCell>
+
+									<TableCell>
+										<span className="cursor-pointer font-medium text-red-500 hover:underline">
+											Delete
+										</span>
+									</TableCell>
+
+									<TableCell className="text-right">
+										<Link className="text-teal-500 hover:underline" to={`/update-post/${post._id}`}>
+											Edit
+										</Link>
+									</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
+					</Table>
+
+					{showMore && (
+						<button
+							onClick={handleShowMore}
+							className="w-full self-center py-7 text-sm text-teal-500"
+						>
+							Show more
+						</button>
+					)}
+				</>
 			) : (
 				<p>You have no posts yet!</p>
 			)}
