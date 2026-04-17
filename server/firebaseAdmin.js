683f53f63a6ded2223826a9c368f2b96f443019d
zahid <zaahidrather@gmail.com>
2026-04-17 12:20:25 +0530

    Fix deployment bugs

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -6,11 +6,20 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const serviceAccount = JSON.parse(
-    fs.readFileSync(path.join(__dirname, "serviceAccountKey.json"), "utf8")
-);
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // Production: Parse the JSON string from Environment Variables
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // Local Development: Read the file
+  JSON.parse(
+    fs.readFileSync(path.join(__dirname, "serviceAccountKey.json"), "utf8"),
+  );
+}
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 export default admin;
